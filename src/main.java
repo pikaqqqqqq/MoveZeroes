dc7a0cf99a03cbf8c099abc59adbc795be4aa963
pikaqqqqq <281705236@qq.com>
2018-01-02 13:25:35 +0800

    更新笔记

--- src/main.java
+++ src/main.java
@@ -4,13 +4,14 @@
 public class main {
     public static void main(String[] args) {
         int[] nums = {0, 1, 0, 3, 12};
-        moveZeroes(nums);
+        moveZeroes2(nums);
         for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i]);
         }
     }
 
-    public static void moveZeroes(int[] nums) {
+    //num of operation:nums.length
+    public static void moveZeroes1(int[] nums) {
         if (nums.length < 2) return;
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
@@ -20,6 +21,20 @@ public class main {
         }
         for (; count < nums.length; count++) {
             nums[count] = 0;
+        }
+    }
+
+    //num of operation:2 * num of non-zore
+    //lost of zero
+    public static void moveZeroes2(int[] nums) {
+        if (nums.length < 2) return;
+        int temp;
+        for (int i = 0, j = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                temp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = temp;
+            }
         }
     }
 }
